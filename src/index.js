400d505c5adc3bb659b2be069932ddf4ddcb3fb7
vadzim khomich <vadzim.khomich@activeplatform.com>
2024-08-09 22:17:05 +0300

    done task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,73 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    function decode(expr) {
+    let arrayOfLetters = []
+
+for (let i = 0; i < expr.length; i += 10) {
+    let oneLetter = expr.slice(i, i + 10)
+    arrayOfLetters.push(oneLetter)
+}
+
+let resultString = ''
+
+
+for (let letter of arrayOfLetters) {
+    if (letter === '**********') {
+        resultString += ' '
+        continue
+    }
+
+    let regExp1 = /10/g
+    let regExp2 = /11/g
+    let regExp3 = /0/g
+
+    let c = letter.replace(regExp1, ".")
+    let d = c.replace(regExp2, "-")
+    let x = d.replace(regExp3, '')
+    resultString += x
+}
+return resultString
+}
 }
 
 module.exports = {
     decode
+}
+function decode(expr) {
+    let arrayOfLetters = []
+
+for (let i = 0; i < expr.length; i += 10) {
+    let oneLetter = expr.slice(i, i + 10)
+    arrayOfLetters.push(oneLetter)
+}
+
+let morseArray = []
+
+
+for (let letter of arrayOfLetters) {
+    if (letter === '**********') {
+        morseArray.push(' ')
+        continue
+    }
+
+    let regExp1 = /10/g
+    let regExp2 = /11/g
+    let regExp3 = /0/g
+
+    let c = letter.replace(regExp1, ".")
+    let d = c.replace(regExp2, "-")
+    let x = d.replace(regExp3, '')
+    morseArray.push(x)
+}
+
+let resultSentence = morseArray.map( (el) => {
+    if (el === ' ') {
+        return ' '
+    }
+    return MORSE_TABLE[el]
+})
+
+return resultSentence.join('')
+
 }
